f1fcb69804126d343ac833292130582fd655720f
Safique Ahmed Faruque <69505988+s-a-faruque@users.noreply.github.com>
2026-08-21 13:52:06 +0000

    portfolio

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("js");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
